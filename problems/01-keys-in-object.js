351c8fe49f23f3870ea1df1a8f922570179ca988
MariaChesnokova <88170491+MariaChesnokova@users.noreply.github.com>
2023-10-23 12:28:44 -0400

    Update 01-keys-in-object.js

--- problems/01-keys-in-object.js
+++ problems/01-keys-in-object.js
@@ -13,7 +13,11 @@ keysInObject(foods); // => ["apple", "lemon", "mango"]
 ***********************************************************************/
 
 function keysInObject(obj) {
-  // Your code here
+    let keysArray = [];
+  for (let key in obj) {
+    keysArray.push(key);
+  }
+  return keysArray;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
