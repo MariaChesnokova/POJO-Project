e2e2ed1ecf292c170d86ec32ad874b172b3c4032
MariaChesnokova <88170491+MariaChesnokova@users.noreply.github.com>
2023-10-23 12:34:07 -0400

    Update 11-spread-it-out.js

--- problems/11-spread-it-out.js
+++ problems/11-spread-it-out.js
@@ -11,7 +11,8 @@ spreadItOut(["apple", "banana"], [1,2,3]); // => ["apple", "banana", 1, 2, 3];
 ***********************************************************************/
 
 function spreadItOut(array1, array2) {
-  // Your code here
+  let array3 = [...array1, ...array2];
+  return array3;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
